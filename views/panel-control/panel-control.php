3c1dd24face521300570a06f4a5fdef7abd02949
Flor Morrone <mfmorrone@gmail.com>
2021-09-26 21:20:54 -0300

    sidebar header

--- views/panel-control/panel-control.php
+++ views/panel-control/panel-control.php
@@ -5,6 +5,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <link rel="stylesheet" href="styles.css">
+<link rel="stylesheet" href="../shared/header-loggedin/styles.css">
 
 <?php require "../shared/header-loggedin/header-loggedin.php" ?>
 	<!--CONTENIDO-->
@@ -14,6 +15,7 @@
 			<button class="btn btn-panel-control">Administrar Niveles</button>
 			<button class="btn btn-panel-control">Administrar Noticias</button>
 			<button class="btn btn-panel-control">Administrar Cuestionarios</button>
+			<button class="btn btn-panel-control">Administrar Centros de reciclaje</button>
 			<button class="btn btn-panel-control">Administrar Cupones</button>
 		</div>
 	</div>
--- views/shared/header-loggedin/header-loggedin.php
+++ views/shared/header-loggedin/header-loggedin.php
@@ -1,10 +1,24 @@
-<link rel="stylesheet" href="styles.css">
+<!--Esta view es con el sólo fin de que se vea el maquetado completo. 
+Se quitará al agregar funcionalidad al header para que muestre botones segun este activa la sesión o no-->
+
+<!-- Sidebar -->
+<div class="sidebar" style="display: none" id="mySidebar">
+	<ul>
+<button id="sidebarCloseBtn" class="btn closeBtn">&times;</button>
+	<li><a href="#">Administrar niveles</a></li>
+  <li><a href="#">Administrar  noticias</a></li>
+  <li><a href="#">Administrar cuestionarios</a></li>
+  <li><a href="#">Administrar centros de reciclaje</a></li>
+	<li><a href="#">Administrar cupones</a></li>
+  <li><input type="submit" class="btn w-100" id="btnCerrarSesion" value="Cerrar sesión"/></li>	
+	</ul>
+</div>
 <!--HEADER-->
 	<header>
 		<nav class="navbar ">
 			<div class="container-fluid">
 				<a href="/glow-app/"><img width="70px" heigth="70px" src="../../assets/eco-friendly.png" alt="logo" class="logo" ></a>
-				<input type="submit" class="btn btn-danger" id="btnCerrarSesion" value="Cerrar sesión"/>
+  			<button class="btn " id="sidebarMenuBtn" >☰</button>
 			</div>
 		</nav>
 	</header>
@@ -15,5 +29,16 @@
 		console.log("click")
 		window.location.replace("../../index.php");
 	})
+	document.getElementById("sidebarMenuBtn").addEventListener("click", w3_open);
+	document.getElementById("sidebarCloseBtn").addEventListener("click", w3_close);
+
+	function w3_open() {
+		document.getElementById("mySidebar").style.width = "100%";
+		document.getElementById("mySidebar").style.display = "block";
+	}
+
+	function w3_close() {
+		document.getElementById("mySidebar").style.display = "none";
+	}
 }
 </script>
